62cebb660569b5dd656246e20a84a29d1ade71e9
Kirill Olenev <olenyov.kirill@me.com>
2025-12-28 11:22:04 +0900

    ios initial sharing

--- koordxtractkmp/iosApp/KoordXTract/ShareViewController.swift
+++ koordxtractkmp/iosApp/KoordXTract/ShareViewController.swift
@@ -7,24 +7,70 @@
 
 import UIKit
 import Social
+import UniformTypeIdentifiers
+import os.log
 
 class ShareViewController: SLComposeServiceViewController {
+    
+    let logger = OSLog(subsystem: "com.shashlikmap.koordxtract", category: "shareextension")
 
+
+    private let scheme = "koordxtract"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("kiol view did load")
+        os_log("viewDidLoad", log: logger, type: .info)
+        guard let extensionItems = extensionContext?.inputItems as? [NSExtensionItem],
+              let attachments = extensionItems.compactMap({ $0.attachments }).flatMap({ $0 }) as? [NSItemProvider],
+              let itemProvider = attachments.first(where: { $0.hasItemConformingToTypeIdentifier(UTType.url.identifier) }) else {
+            os_log("[KoordXTract Extension] - URL ItemProvider is missing", log: logger, type: .error)
+//            extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+            return
+        }
+        itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier) { [weak self] data, error in
+            guard let url = data as? URL else {
+                os_log("[KoordXTract Extension] - Invalid URL", log: self!.logger, type: .error)
+                //                self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+                return
+            }
+            
+            print("[KoordXTract Extension] - Shared URL \(url)")
+            self?.openContainerApp(with: url)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        os_log("viewDidAppear", log: logger, type: .info)
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    }
+    
+    private func openContainerApp(with sharedURL: URL) {
+        guard var urlComponents = URLComponents(string: scheme + "://share") else { return }
+        let queryItem = URLQueryItem(name: "link", value: sharedURL.absoluteString.removingPercentEncoding)
+        urlComponents.queryItems = [queryItem]
+        guard let url = urlComponents.url else { return }
+
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                print("app open fron extension \(application)")
+                application.open(url, options: [:], completionHandler: nil)
+                break
+            }
+            responder = responder?.next
+        }
+    }
     override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
         return true
     }
 
     override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
 
     override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
         return []
     }
-
 }
--- koordxtractkmp/iosApp/iosApp/iOSApp.swift
+++ koordxtractkmp/iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,24 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL { url in
+                appDelegate.handleURL(url)
+            }
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        handleURL(url)
+        return true
+    }
+    
+    func handleURL(_ url: URL) {
+        print("DEBUG: AppDelegate - handleURL called with: \(url)")
     }
 }
